c6c6b542aef12611bc4f917ed66f79817c8bdba8
pimathbrainiac <pimathbrainiac@github.com>
2014-12-31 20:55:31 -0500

    scaled sprites and scrolling screentest

--- nReuben.c
+++ nReuben.c
@@ -2,21 +2,20 @@
 #include	"RQ1forestisland.h"
 #include	"n2DLib/n2DLib.h"
 
-#define		TILE_WIDTH	8
-#define		TILE_HEIGHT	8
+#define		TILE_WIDTH	16
+#define		TILE_HEIGHT	16
 
 #define		K_ESC		isKeyPressed(KEY_NSPIRE_ESC)
 #define		K_ENTER		isKeyPressed(KEY_NSPIRE_ENTER)
 #define		K_TAB		isKeyPressed(KEY_NSPIRE_TAB)
-#define		K_A			isKeyPressed(KEY_NSPIRE_A)
-#define		K_7			isKeyPressed(KEY_NSPIRE_7)
-#define		K_9			isKeyPressed(KEY_NSPIRE_9)
-#define		K_8			isKeyPressed(KEY_NSPIRE_8)
-#define		K_5			isKeyPressed(KEY_NSPIRE_5)
-#define		K_4			isKeyPressed(KEY_NSPIRE_4)
-#define		K_6			isKeyPressed(KEY_NSPIRE_6)
+#define		K_UP		isKeyPressed(KEY_NSPIRE_UP)
+#define		K_DOWN		isKeyPressed(KEY_NSPIRE_DOWN)
+#define		K_LEFT		isKeyPressed(KEY_NSPIRE_LEFT)
+#define		K_RIGHT		isKeyPressed(KEY_NSPIRE_RIGHT)
 
 Rect src_rect;
+int pos_x = 0;
+int pos_y = 0;
 
 void draw_tile(unsigned short *tileset, int tile_num, int x, int y)
 {
@@ -28,9 +27,16 @@ void draw_tile(unsigned short *tileset, int tile_num, int x, int y)
 void draw_tile_map(void)
 {
     int i, j;
-    for(i = 0; i < MAP_HEIGHT; ++i)
-        for(j = 0; j < MAP_WIDTH; ++j)
-            draw_tile(RQ1forestisland, RQ1forestisland_map[i][j], j * TILE_WIDTH, i * TILE_HEIGHT);
+    for(i = pos_y; i < (8 + pos_y); ++i)
+        for(j = pos_x; j < (12 + pos_x); ++j)
+            draw_tile(RQ1forestisland, RQ1forestisland_map[i][j], (j - pos_x + 4) * TILE_WIDTH, (i - pos_y + 3) * TILE_HEIGHT);
+}
+
+void draw_everything()
+{
+	clearBufferB();
+	draw_tile_map();
+	updateScreen();
 }
 
 int main(void)
@@ -49,6 +55,34 @@ int main(void)
 	{
 		if(K_ESC)
 			keep_playing = 0;
+		if(K_UP)
+		{
+			if(pos_y > 0)
+				pos_y--;
+			sleep(250);
+			draw_everything();
+		}
+		if(K_DOWN)
+		{
+			if(pos_y < MAP_HEIGHT - 8)
+				pos_y++;
+			sleep(250);
+			draw_everything();
+		}
+		if(K_LEFT)
+		{
+			if(pos_x > 0)
+				pos_x--;
+			sleep(250);
+			draw_everything();
+		}
+		if(K_RIGHT)
+		{
+			if(pos_x < MAP_WIDTH - 12)
+				pos_x++;
+			sleep(250);
+			draw_everything();
+		}
 	}
 	deinitBuffering();
 }
